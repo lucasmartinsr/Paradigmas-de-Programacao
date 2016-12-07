e94eaf78bb7e4cc3220fa00f41fdc65b1d9184f6
Sávio Cordeiro <saviocord@gmail.com>
2016-12-07 11:38:36 -0200

    Exercicio 10 da lista 5
    código em C do exercício 10

---
+++ Prog10Lista5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int fun(){
+	return 1;
+}
+int main(){
+	int a, b;
+	a = 10;
+	b = a + fun();
+	printf("Com a chamada a função pela direita, ");
+	printf("b = %d\n", b);
+	a = 10;
+	b = fun() + a;
+	printf("Com a chamada a função pela esquerda, ");
+	printf("b = %d\n", b);
+	
+	//resultados:
+	//Com a chamada a função pela direita, b = 11
+	//Com a chamada a função pela esquerda, b = 11
+}
